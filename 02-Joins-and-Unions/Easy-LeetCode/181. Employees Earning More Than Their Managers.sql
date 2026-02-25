7d4b911f12a91197f25a26b0833cc5d5b7d261a0
RehanSaqib <rehan118002@gmail.com>
2026-02-25 20:22:52 +0500

    joins and window function easy med leetcode

--- 02-Joins-and-Unions/Easy-LeetCode/181. Employees Earning More Than Their Managers.sql
+++ 02-Joins-and-Unions/Easy-LeetCode/181. Employees Earning More Than Their Managers.sql
@@ -12,4 +12,4 @@ Return the result table in any order.
 SELECT e.name AS Employee
 FROM Employee e
 INNER JOIN Employee m ON e.managerId = m.id
-WHERE e.salary > m.salary;
+WHERE e.salary > m.salary; 
