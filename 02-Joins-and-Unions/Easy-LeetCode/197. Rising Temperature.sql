3650d3c6de1095f7163bb1da8d88977dfbdade86
RehanSaqib <rehan118002@gmail.com>
2026-02-28 10:04:07 +0500

    joins easy leetcode no 197

---
+++ 02-Joins-and-Unions/Easy-LeetCode/197. Rising Temperature.sql
@@ -0,0 +1,12 @@
+/*
+Problem: 197. Rising Temperature
+Link: https://leetcode.com/problems/rising-temperature/
+Difficulty: Easy
+Concept: Self-Join, Date Comparison (DATEDIFF)
+*/
+
+SELECT w1.id
+FROM Weather w1
+INNER JOIN Weather w2 
+    ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
