7bc01f33f779d59cc8c50964fdd57038bbabe64a
RehanSaqib <rehan118002@gmail.com>
2026-05-05 20:10:37 +0500

    Added solution for LeetCode 1084 - Sales only in Q1 using MIN/MAX

---
+++ 03-Aggregations-and-Grouping/Easy-LeetCode/1084. Sales Analysis III.sql
@@ -0,0 +1,22 @@
+/*
+Problem: 1084. Sales Analysis III
+Link: https://leetcode.com/problems/sales-analysis-iii/
+Difficulty: Easy
+
+Concepts:
+- JOIN
+- GROUP BY
+- MIN / MAX
+- HAVING
+
+Description:
+Find products that were ONLY sold in the first quarter of 2019
+(i.e., between 2019-01-01 and 2019-03-31).
+*/
+
+SELECT s.product_id, p.product_name
+FROM Product p
+JOIN Sales s ON p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01'
+   AND MAX(s.sale_date) <= '2019-03-31';
