39513656e3077c9d2fd23b327ec058c137dfcc12
RehanSaqib <rehan118002@gmail.com>
2026-05-12 22:50:22 +0500

    window function easy leetcode 197

--- 05-Window-Functions/Easy-LeetCode/197. Rising Temperature.sql
+++ 05-Window-Functions/Easy-LeetCode/197. Rising Temperature.sql
@@ -6,16 +6,16 @@ Concept: Window Functions (LAG), CTE, Date Validation (DATEDIFF)
 
 Description:
 Find all dates' Id with higher temperatures compared to its previous dates (yesterday).
-This solution uses LAG to avoid a self-join and DATEDIFF to handle non-consecutive dates.
+This solution uses LAG to avoid a self-join  and DATEDIFF to handle non-consecutive dates.
 */
 
-WITH prev_temp AS (
+WITH prev_temp  AS (
     SELECT 
         id,
         recordDate,
         temperature AS current_temp,
         LAG(temperature) OVER (ORDER BY recordDate) AS prev_day_temp,
-        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date
+        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_datee
     FROM Weather 
 )
 SELECT id
