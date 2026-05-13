b08055c04552bbcf4f12a7244762be970bb38997
RehanSaqib <rehan118002@gmail.com>
2026-05-13 22:26:16 +0500

    window function problem 511

--- 05-Window-Functions/Easy-LeetCode/511. Game Play Analysis I.sql
+++ 05-Window-Functions/Easy-LeetCode/511. Game Play Analysis I.sql
@@ -9,10 +9,10 @@ Find the first login date for each player using Window Functions.
 */
 
 WITH ranked_date AS (
-    SELECT player_id,event_date AS first_login,
+    SELECT player_id,event_ date AS first_login,
     DENSE_RANK () OVER (PARTITION BY player_id ORDER BY event_date ASC) as rnk
     FROM Activity
 )
-SELECT player_id,first_login 
+SELECT player_id,first_login  
 FROM ranked_date
 WHERE rnk = 1 ;
