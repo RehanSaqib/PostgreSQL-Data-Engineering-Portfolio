148e9b8f884d5e1e4aec89e2412f8a4d99cb7fe3
RehanSaqib <rehan118002@gmail.com>
2026-05-14 22:12:58 +0500

    1141 user activity 03 easy leetcode

--- 03-Aggregations-and-Grouping/Easy-LeetCode/1141. User Activity for the Past 30 Days I.sql
+++ 03-Aggregations-and-Grouping/Easy-LeetCode/1141. User Activity for the Past 30 Days I.sql
@@ -14,7 +14,7 @@ A user is active if they have at least one activity on that day.
 */
 
 SELECT activity_date AS day,
-       COUNT(DISTINCT user_id) AS active_users
+       COUNT (DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-GROUP BY activity_date;
+WHERE activity_date  BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date ;
