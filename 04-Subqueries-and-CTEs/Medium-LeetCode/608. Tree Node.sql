99243594aaa05d63a0e89ca96a8aa8b13da43af8
RehanSaqib <rehan118002@gmail.com>
2026-05-24 22:44:48 +0500

    Added solution for LeetCode 608 Tree Node

---
+++ 04-Subqueries-and-CTEs/Medium-LeetCode/608. Tree Node.sql
@@ -0,0 +1,29 @@
+/*
+Problem: 608. Tree Node
+Link: https://leetcode.com/problems/tree-node/
+Difficulty: Medium
+Concept: CASE WHEN, SUBQUERY
+
+Description:
+Report the type of each node in the tree:
+- "Root"  -> if p_id is NULL
+- "Leaf"  -> if node has no children
+- "Inner" -> otherwise
+*/
+
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL
+        THEN 'Root'
+
+        WHEN id NOT IN (
+            SELECT p_id
+            FROM Tree
+            WHERE p_id IS NOT NULL
+        )
+        THEN 'Leaf'
+
+        ELSE 'Inner'
+    END AS type
+FROM Tree;
