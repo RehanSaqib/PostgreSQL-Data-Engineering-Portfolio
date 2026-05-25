ccd3c8045c18f6cc875e2f06479fe125271ee2c3
RehanSaqib <rehan118002@gmail.com>
2026-05-25 23:43:20 +0500

    hard leetcode 185 window function

--- 05-Window-Functions/Hard-LeetCode/185. Department Top Three Salaries.sql
+++ 05-Window-Functions/Hard-LeetCode/185. Department Top Three Salaries.sql
@@ -11,10 +11,10 @@ for that department.
 */
 
 WITH max_salary AS (
-    SELECT  e.name AS Employee,e.salary AS Salary , d.name AS Department,
+    SELECT  e.name AS Employee,e.salary AS Salary  , d.name AS Department,
     DENSE_RANK () OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
-    FROM Employee e 
-    INNER JOIN Department d ON e.departmentId = d.id
+     FROM Employee e 
+    INNER JOIN Department d ON e.departmentId =  d.id
 )
 SELECT Department ,Employee ,  Salary
 FROM max_salary
