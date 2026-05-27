4c40eb0e8182ac37c03452e29ce6a062c116ed1d
RehanSaqib <rehan118002@gmail.com>
2026-05-28 00:10:58 +0500

    620 easy leetcode

--- 01-Basic-Queries/Easy-LeetCode/620. Not Boring Movies.sql
+++ 01-Basic-Queries/Easy-LeetCode/620. Not Boring Movies.sql
@@ -15,6 +15,6 @@ Return the result ordered by rating in descending order.
 
 SELECT *
 FROM Cinema
-WHERE id % 2 = 1
-  AND description != 'boring'
-ORDER BY rating DESC;
+WHERE id %  2 = 1
+  AND description  !=  'boring'
+ORDER BY rating DESC ;
