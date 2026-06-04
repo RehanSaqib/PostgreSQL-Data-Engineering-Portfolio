1be2305f709ba5cc6b655f26d2b44c5ed888e30a
RehanSaqib <rehan118002@gmail.com>
2026-06-04 23:21:54 +0500

    easy leetcode basic 577

--- 01-Basic-Queries/Easy-LeetCode/577. Employee Bonus.sql
+++ 01-Basic-Queries/Easy-LeetCode/577. Employee Bonus.sql
@@ -5,8 +5,8 @@ Difficulty: Easy
 Concept: LEFT JOIN, IS NULL, Filtering
 */
 
-SELECT e.name, b.bonus
-FROM Employee e
-LEFT JOIN Bonus b ON e.empId = b.empId
-WHERE b.bonus < 1000 
-OR b.bonus IS NULL; 
+ SELECT e.name, b.bonus
+ FROM Employee e
+ LEFT JOIN Bonus b ON e.empId = b.empId
+ WHERE b.bonus < 1000 
+ OR b.bonus IS NULL ; 
