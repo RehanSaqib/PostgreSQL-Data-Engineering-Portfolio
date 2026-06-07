8846b68233a9f596de9bc13d4c7e9cc8a946f9cb
RehanSaqib <rehan118002@gmail.com>
2026-06-07 22:20:29 +0500

    586 leetcode easy different approch using df

--- 03-Aggregations-and-Grouping/Easy-LeetCode/586. Customer Placing the Largest Number of Orders.sql
+++ 03-Aggregations-and-Grouping/Easy-LeetCode/586. Customer Placing the Largest Number of Orders.sql
@@ -13,8 +13,24 @@ Description:
 Find the customer_number who has placed the highest number of orders.
 */
 
-SELECT customer_number
-FROM Orders
-GROUP BY customer_number
+SELECT  customer_number
+FROM  Orders
+GROUP BY  customer_number
 ORDER BY COUNT(*) DESC
+LIMIT 1 ;
+
+/*
+Problem: 586. Customer Placing the Largest Number of Orders
+Concept: Derived Table (DF)
+*/
+
+SELECT customer_number
+FROM (
+    SELECT
+        customer_number,
+        COUNT(*) AS total_orders
+    FROM Orders
+    GROUP BY customer_number
+) AS df
+ORDER BY total_orders DESC
 LIMIT 1;
