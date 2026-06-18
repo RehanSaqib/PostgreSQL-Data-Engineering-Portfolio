e6c35da9e4ceb69d660bfbf0bec8afe445809303
RehanSaqib <rehan118002@gmail.com>
2026-06-18 22:59:11 +0500

    1661 average time of process easy leetcode

--- 02-Joins-and-Unions/Easy-LeetCode/1661. Average Time of Process per Machine.sql
+++ 02-Joins-and-Unions/Easy-LeetCode/1661. Average Time of Process per Machine.sql
@@ -21,5 +21,5 @@ JOIN Activity b
    ON a.machine_id = b.machine_id
    AND a.process_id = b.process_id
    WHERE a.activity_type = 'start'
-   AND b.activity_type = 'end'
-   GROUP BY a.machine_id ;
+   AND b.activity_type = ' end '
+   GROUP BY a.machine_id  ;
