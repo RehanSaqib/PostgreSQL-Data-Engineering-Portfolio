90b86cace5c4268238782bc1485d802d5fa2db1a
RehanSaqib <rehan118002@gmail.com>
2026-06-19 21:39:14 +0500

    Added solution for LeetCode 1683 Invalid Tweets

---
+++ 01-Basic-Queries/Easy-LeetCode/1683. Invalid Tweets.sql
@@ -0,0 +1,16 @@
+/*
+Problem: 1683. Invalid Tweets
+Link: https://leetcode.com/problems/invalid-tweets/
+Difficulty: Easy
+Concept: String Functions, LENGTH()
+
+Description:
+Find the IDs of invalid tweets.
+A tweet is invalid if its content
+contains more than 15 characters.
+*/
+
+SELECT
+    tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
