4f7d651557aebc128966e0a543dacd280226693e
RehanSaqib <rehan118002@gmail.com>
2026-06-19 21:56:50 +0500

    Added solution for LeetCode 1729 Find Followers Count

---
+++ 03-Aggregations-and-Grouping/Easy-LeetCode/1729. Find Followers Count.sql
@@ -0,0 +1,18 @@
+/*
+Problem: 1729. Find Followers Count
+Link: https://leetcode.com/problems/find-followers-count/
+Difficulty: Easy
+Concept: GROUP BY, COUNT()
+
+Description:
+For each user, find the total number
+of followers and return the result
+ordered by user_id.
+*/
+
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
