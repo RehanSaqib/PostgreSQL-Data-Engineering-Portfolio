5f36cb08c97bce5651cbcf085a618e7346888cdb
RehanSaqib <rehan118002@gmail.com>
2026-07-01 22:39:12 +0500

    Added window function practice problem Salary Difference From Previous Employee

---
+++ 06-Data-Modeling/Easy-LeetCode/Salary Difference From Previous Employee.sql
@@ -0,0 +1,16 @@
+WITH prev AS
+(
+    SELECT
+        employee_id,
+        employee_name,
+        salary,
+        LAG(salary) OVER (ORDER BY employee_id) AS p
+    FROM Employee
+)
+
+SELECT
+    employee_id,
+    employee_name,
+    salary,
+    p AS previous_salary
+FROM prev;
