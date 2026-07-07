617f105426c17ad433a319ba62fb984978ed1655
RehanSaqib <rehan118002@gmail.com>
2026-07-07 23:49:11 +0500

    most active customer

--- 01-Basic-Queries/Easy-LeetCode/Most Active Customer.sql
+++ 01-Basic-Queries/Easy-LeetCode/Most Active Customer.sql
@@ -4,8 +4,8 @@ Difficulty: Easy
 Concept: GROUP BY, COUNT
 */
 
-SELECT customer_id
-FROM Orders
-GROUP BY customer_id
-ORDER BY COUNT(*) DESC
-LIMIT 1;
+ SELECT customer_id 
+ FROM Orders
+ GROUP BY customer_id
+ ORDER BY COUNT(*) DESC
+ LIMIT 1 ;
