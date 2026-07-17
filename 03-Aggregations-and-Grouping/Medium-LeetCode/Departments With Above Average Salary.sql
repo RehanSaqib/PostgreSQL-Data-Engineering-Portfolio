83f897ca999df9b8b79fbd9a6612ff5c6bf9f4af
RehanSaqib <rehan118002@gmail.com>
2026-07-17 23:13:41 +0500

    Added medium aggregation practice Departments With Above Average Salary with question

--- 03-Aggregations-and-Grouping/Medium-LeetCode/Departments With Above Average Salary.sql
+++ 03-Aggregations-and-Grouping/Medium-LeetCode/Departments With Above Average Salary.sql
@@ -1,5 +1,49 @@
 /*
 Problem: Departments With Above Average Salary
+
+Difficulty: Medium
+Concept: GROUP BY, AVG(), HAVING, Subquery
+
+Description:
+A company wants to identify departments
+whose average salary is greater than
+the average salary of all employees.
+
+Return:
+
+1. department
+2. average_salary
+
+Round the average salary to 2 decimal places.
+
+Order the result by average_salary DESC.
+
+Employee Table
+
++-------------+---------------+------------+--------+
+| employee_id | employee_name | department | salary |
++-------------+---------------+------------+--------+
+| 1           | Ali           | IT         | 70000  |
+| 2           | Ahmed         | IT         | 60000  |
+| 3           | Bilal         | HR         | 50000  |
+| 4           | Hamza         | HR         | 45000  |
+| 5           | Rehan         | Sales      | 80000  |
+| 6           | Asad          | Sales      | 90000  |
+| 7           | Usman         | Finance    | 55000  |
+| 8           | Saad          | Finance    | 50000  |
++-------------+---------------+------------+--------+
+
+Expected Output
+
++------------+----------------+
+| department | average_salary |
++------------+----------------+
+| Sales      | 85000.00       |
+| IT         | 65000.00       |
++------------+----------------+
+*/
+/*
+Problem: Departments With Above Average Salary
 Difficulty: Medium
 Concept: GROUP BY, AVG(), HAVING, Subquery
 */
