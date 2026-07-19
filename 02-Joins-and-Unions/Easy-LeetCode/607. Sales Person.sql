e5ae743a8be92002d34df497cfba144d852cabed
RehanSaqib <rehan118002@gmail.com>
2026-07-19 23:20:59 +0500

    joins easy leetcode

--- 02-Joins-and-Unions/Easy-LeetCode/607. Sales Person.sql
+++ 02-Joins-and-Unions/Easy-LeetCode/607. Sales Person.sql
@@ -16,7 +16,7 @@ SELECT name
 FROM SalesPerson
 WHERE sales_id NOT IN (
     SELECT o.sales_id
-    FROM Orders o
-    JOIN Company c ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
+     FROM Orders o
+     JOIN Company c ON o.com_id = c.com_id
+     WHERE c.name = 'RED'
 );
