8bed83a50dfd4e94db87dfa6e6f7710a88c10575
RehanSaqib <rehan118002@gmail.com>
2026-07-28 23:25:12 +0500

    highest and lowest salary easy leetcode

--- 03-Aggregations-and-Grouping/Easy-LeetCode/Highest and Lowest Salary by Department.sql
+++ 03-Aggregations-and-Grouping/Easy-LeetCode/Highest and Lowest Salary by Department.sql
@@ -9,9 +9,9 @@ for each department.
 */
 
 SELECT
-    department,
-    MAX(salary) AS highest_salary,
-    MIN(salary) AS lowest_salary
+     department,
+     MAX(salary) AS highest_salary,
+     MIN(salary) AS lowest_salary
 FROM Employee
 GROUP BY department
 ORDER BY department;
