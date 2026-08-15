6e2113f46b6df28f18fa6b492ccc6ec007c3782d
RehanSaqib <rehan118002@gmail.com>
2026-08-16 00:00:54 +0500

    med leetcode 585

--- 03-Aggregations-and-Grouping/Medium-LeetCode/585. Investments in 2016.sql
+++ 03-Aggregations-and-Grouping/Medium-LeetCode/585. Investments in 2016.sql
@@ -1,11 +1,11 @@
 /*
 Problem: 585. Investments in 2016
 Link: https://leetcode.com/problems/investments-in-2016/
-Difficulty: Medium
-
+D  ifficulty: Medium
+  
 Concepts:
-- GROUP BY
-- HAVING
+- GROUP  BY
+- HAVING  
 - Subqueries
 - Aggregation
 
